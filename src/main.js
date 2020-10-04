cd5cfde3e4e0a0b5eefc7a7c05ddb3f6e557dfac
lkw199711 <lkw199711@163.com>
2020-10-04 10:52:56 +0800

    feat:密钥与请求

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./index.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
@@ -7,7 +7,9 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
+
+Vue.prototype.$user = '1234';
