e991fe33bd4bc6bc4394bd2c261a9c373f6ff2af
lkw199711 <lkw199711@163.com>
2020-10-04 12:20:38 +0800

    feat:配置ps-rem单位转换

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     lintOnSave: false,
+    // 开发环境代理
     devServer: {
         proxy: {//配置代理
             '/cms': {
@@ -10,6 +11,26 @@ module.exports = {
                     '^/cms': ''
                 }
             }
+        }
+    },
+    plugins: {
+        'postcss-import': {},
+        'postcss-url': {},
+        autoprefixer: {
+            // 筛选浏览器
+            browsers: ['Android >= 4.0', 'iOS >= 8'],
+        },
+        'postcss-pxtorem': {
+            // 根元素,1rem的大小
+            rootValue: 32,
+            // 哪些需要进行px转rem
+            propList: ['*'],
+            // 排除哪些开头的如 .weui-button 等等
+            selectorBlackList: [],
+            // 最小转换，如低于 4px的不会进行转成rem
+            minPixelValue: 4,
+            // re单位的小数位数上限
+            unitPrecision: 10,
         }
     }
 };
